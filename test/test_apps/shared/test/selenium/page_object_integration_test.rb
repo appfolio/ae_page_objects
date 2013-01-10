530ed2e2b748013885c2add566cbff6f567caa57
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2013-01-09 21:00:46 -0800

    adding document tracking test and updating signature to initialize()

--- test/test_apps/shared/test/selenium/page_object_integration_test.rb
+++ test/test_apps/shared/test/selenium/page_object_integration_test.rb
@@ -123,4 +123,39 @@ class PageObjectIntegrationTest < Selenium::TestCase
     puts e.backtrace.join("\n")
     raise e      
   end
+  
+  def test_document_tracking
+    author = TestApp::PageObjects::Authors::NewPage.visit
+    assert_false author.stale?
+    
+    visit("/books/new")
+    assert_false author.stale?
+    
+    book = TestApp::PageObjects::Books::NewPage.new
+    assert author.stale?
+    assert_false book.stale?
+    
+    author = TestApp::PageObjects::Authors::NewPage.visit
+    assert_false author.stale?
+    assert book.stale?
+
+    book = TestApp::PageObjects::Books::NewPage.visit
+    assert author.stale?
+    assert_false book.stale?
+    
+    author = TestApp::PageObjects::Authors::NewPage.visit
+    assert_false author.stale?
+    assert book.stale?
+    
+    visit("/authors/new")
+    assert_false author.stale?
+    assert book.stale?
+    
+    assert_raises AePageObjects::LoadingFailed do
+      TestApp::PageObjects::Books::NewPage.new
+    end
+    
+    assert_false author.stale?
+    assert book.stale?
+  end
 end
