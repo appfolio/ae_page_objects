481d5dce620d8a266d8d22fa0af81bf509294806
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2013-07-10 16:54:40 -0700

    refactoring application_router to support different rails versions

--- lib/ae_page_objects/core/application_router.rb
+++ lib/ae_page_objects/core/application_router.rb
@@ -1,60 +1,103 @@
 module AePageObjects
   class ApplicationRouter
+
+    module Recognizer
+      class Rails23
+
+      end
+
+      class Rails3
+        def recognizes?(path, url)
+          url, router = url_and_router(url)
+
+          ["GET", "PUT", "POST", "DELETE", "PATCH"].each do |method|
+            router.recognize(request_for(url, method)) do |route, matches, params|
+              return true if route.name.to_s == path.to_s
+            end
+          end
+
+          false
+        end
+
+        def generate_path(named_route, *args)
+          if routes.respond_to?("#{named_route}_path")
+            routes.send("#{named_route}_path", *args)
+          end
+        end
+
+      private
+
+        def request_for(url, method)
+          Rails.application.routes.request_class.new(env_for(url, method))
+        end
+
+        def env_for(url, method)
+          begin
+            Rack::MockRequest.env_for(url, {:method => method})
+          rescue URI::InvalidURIError => e
+            raise ActionController::RoutingError, e.message
+          end
+        end
+
+        def url_and_router(url)
+          url = Rack::Mount::Utils.normalize_path(url) unless url =~ %r{://}
+          router = Rails.application.routes.set
+
+          [url, router]
+        end
+
+        def routes
+          @routes ||= begin
+            routes_class = Class.new do
+              include Rails.application.routes.url_helpers
+            end
+            routes_class.new
+          end
+        end
+      end
+
+      class Rails32 < Rails3
+
+      private
+        def url_and_router(url)
+          url = Journey::Router::Utils.normalize_path(url) unless url =~ %r{://}
+          router = Rails.application.routes.router
+
+          [url, router]
+        end
+      end
+    end
+
     def path_recognizes_url?(path, url)
       if path.is_a?(String)
         path.sub(/\/$/, '') == url.sub(/\/$/, '')
       elsif path.is_a?(Symbol)
-        url, router = url_and_router(url)
-
-        ["GET", "PUT", "POST", "DELETE", "PATCH"].each do |method|
-          router.recognize(request_for(url, method)) do |route, matches, params|
-            return true if route.name.to_s == path.to_s
-          end
-        end
-        false
+        recognizer.recognizes?(path, url)
       end
     end
 
     def generate_path(named_route, *args)
-      return named_route if named_route.is_a?(String)
-
-      if routes.respond_to?("#{named_route}_path")
-        routes.send("#{named_route}_path", *args)
+      if named_route.is_a?(String)
+        named_route
+      else
+        recognizer.generate_path(named_route, *args)
       end
     end
 
   private
-  
-    def request_for(url, method)
-      Rails.application.routes.request_class.new(env_for(url, method))
-    end
-  
-    def env_for(url, method)
-      begin
-        Rack::MockRequest.env_for(url, {:method => method})
-      rescue URI::InvalidURIError => e
-        raise ActionController::RoutingError, e.message
-      end
-    end
-  
-    def url_and_router(url)
-      if Rails.version =~ /^3.[01]/ 
-        url = Rack::Mount::Utils.normalize_path(url) unless url =~ %r{://}
-        router = Rails.application.routes.set
-      else
-        url = Journey::Router::Utils.normalize_path(url) unless url =~ %r{://}
-        router = Rails.application.routes.router
-      end
-      
-      [url, router]
-    end
 
-    def routes
-      @routes ||= begin
-        routes_class = Class.new do
-          include Rails.application.routes.url_helpers
+    def recognizer
+      @recognizer ||= begin
+        if Rails.version =~ /^2\.3/
+          Recognizer::Rails23.new
+        elsif Rails.version =~ /^3\.[01]/
+          Recognizer::Rails3.new
+        elsif Rails.version =~ /^3\.2/
+          Recognizer::Rails32.new
+        else
+          warn "[WARNING]: AePageObjects is not tested against Rails #{Rails.version} and may behave in an undefined manner."
+          Recognizer::Rails32.new
         end
-        routes_class.new
       end
     end
   end
