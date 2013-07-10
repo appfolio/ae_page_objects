1ddbba40651fa5242abcc9f70b56a736fb9b172b
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2013-07-10 16:35:57 -0700

    fixing up selenium_helper for rails 2.3 app

---
+++ test/test_apps/2.3/test/selenium_helper.rb
@@ -0,0 +1,37 @@
+ENV['RAILS_ENV'] = 'test'
+
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..')
+
+require "config/environment"
+
+# Run the migrations
+ActiveRecord::Migrator.migrate("#{Rails.root}/db/migrate")
+
+require "test_help"
+
+require 'capybara/dsl'
+require 'capybara/rails'
+
+Dir["../../test_helpers/**/*.rb"].each {|f| require f}
+
+module Selenium
+  class TestCase < ActiveSupport::TestCase
+    include Capybara::DSL
+    include AfCruft
+
+    self.use_transactional_fixtures = true
+    self.use_instantiated_fixtures  = false
+
+    fixtures :all
+
+    #include Rails.application.routes.url_helpers
+  end
+end
+
+Capybara.configure do |config|
+  config.default_driver    = :selenium
+  config.default_wait_time = 5
+end
+
+require "test/page_objects"
+
