11b013549cc0b63cef9172889aae711c9c1f252b
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2013-10-26 01:40:31 -0700

    some small nits to collection

--- test/unit/collection_test.rb
+++ test/unit/collection_test.rb
@@ -17,7 +17,7 @@ module AePageObjects
       parent_node.expects(:find).with("#18_holder").returns(magazine_node)
 
       magazine = clip.new(parent, :name => "18_holder", :item_locator => ".some_class")
-      assert_equal ".//*[contains(concat(' ', normalize-space(@class), ' '), ' some_class ')]", magazine.item_xpath
+      assert_equal ".//*[contains(concat(' ', normalize-space(@class), ' '), ' some_class ')]", magazine.send(:item_xpath)
     end
 
     def test_empty
@@ -35,7 +35,7 @@ module AePageObjects
 
       magazine = clip.new(parent, :name => "18_holder")
       
-      magazine_node.stubs(:all).with(:xpath, magazine.item_xpath).returns([])
+      magazine_node.stubs(:all).with(:xpath, magazine.send(:item_xpath)).returns([])
 
       assert_equal 0, magazine.size
       
@@ -67,12 +67,12 @@ module AePageObjects
       
       bullet1_stub = mock
       bullet2_stub = mock
-      magazine_node.stubs(:all).with(:xpath, magazine.item_xpath).returns([bullet1_stub, bullet2_stub])
+      magazine_node.stubs(:all).with(:xpath, magazine.send(:item_xpath)).returns([bullet1_stub, bullet2_stub])
 
       assert_equal 2, magazine.size
       
-      magazine_node.expects(:find).with(:xpath, "#{magazine.item_xpath}[1]").returns(bullet1_stub)
-      magazine_node.expects(:find).with(:xpath, "#{magazine.item_xpath}[2]").returns(bullet2_stub)
+      magazine_node.expects(:find).with(:xpath, "#{magazine.send(:item_xpath)}[1]").returns(bullet1_stub)
+      magazine_node.expects(:find).with(:xpath, "#{magazine.send(:item_xpath)}[2]").returns(bullet2_stub)
       each_block_call_count = 0
       magazine.each do |bullet|
         bullet.name
@@ -80,16 +80,16 @@ module AePageObjects
       end
       assert_equal 2, each_block_call_count
       
-      magazine_node.expects(:find).with(:xpath, "#{magazine.item_xpath}[1]").times(2).returns(bullet1_stub)
+      magazine_node.expects(:find).with(:xpath, "#{magazine.send(:item_xpath)}[1]").times(2).returns(bullet1_stub)
       assert_equal bullet1_stub, magazine.at(0).node
       assert_equal bullet1_stub, magazine.first.node
             
-      magazine_node.expects(:find).with(:xpath, "#{magazine.item_xpath}[2]").times(2).returns(bullet2_stub)
+      magazine_node.expects(:find).with(:xpath, "#{magazine.send(:item_xpath)}[2]").times(2).returns(bullet2_stub)
       assert_equal bullet2_stub, magazine.at(1).node
       assert_equal bullet2_stub, magazine.last.node
       
-      magazine_node.expects(:find).with(:xpath, "#{magazine.item_xpath}[1]").returns(bullet1_stub)
-      magazine_node.expects(:find).with(:xpath, "#{magazine.item_xpath}[2]").returns(bullet2_stub)
+      magazine_node.expects(:find).with(:xpath, "#{magazine.send(:item_xpath)}[1]").returns(bullet1_stub)
+      magazine_node.expects(:find).with(:xpath, "#{magazine.send(:item_xpath)}[2]").returns(bullet2_stub)
       assert_equal [bullet1_stub, bullet2_stub], magazine.map(&:node)
       
       assert_equal nil, magazine.at(1000)
