883254ab1e3e58d4d6ff3c817e50fba94f352a2d
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2013-10-28 13:33:19 -0700

    adding Document.find

--- lib/ae_page_objects/document.rb
+++ lib/ae_page_objects/document.rb
@@ -2,6 +2,52 @@ module AePageObjects
   class Document < Node
     include Concerns::Visitable
 
+    class << self
+
+      def find(&extra_condition)
+        original_window = AePageObjects::Window.current
+
+        # Loop through all the windows and attempt to instantiate the Document. Continue to loop around
+        # until finding a Document that can be instantiated or timing out.
+        Capybara.wait_until do
+          AePageObjects::Window.all.each do |window|
+            window.switch_to
+
+            if inst = attempt_to_load(&extra_condition)
+              break inst
+            end
+          end
+        end
+
+      rescue Capybara::TimeoutError
+        original_window.switch_to
+
+        all_windows = AePageObjects::Window.all.map do |window|
+          {:window_handle => window.handle, :document => window.current_document.try(:name) }
+        end
+
+        raise PageNotFound, "Couldn't find page #{self.name} in any of the open windows: #{all_windows.inspect}"
+      end
+
+    private
+      def attempt_to_load(&extra_condition)
+        inst = new
+
+        if extra_condition.nil? || inst.instance_eval(&extra_condition)
+          return inst
+        end
+
+        nil
+      rescue AePageObjects::LoadingFailed
+        # These will happen from the new() call above.
+        nil
+      end
+
+      def site
+        @site ||= AePageObjects::Site.from(self)
+      end
+    end
+
     attr_reader :window
     
     def initialize
@@ -13,13 +59,6 @@ module AePageObjects
     
     def document
       self
-    end
-    
-    class << self
-    private
-      def site
-        @site ||= AePageObjects::Site.from(self)
-      end
     end
   end
 end
