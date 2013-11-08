9581f6f6d15265c5cf62c6d5318c06e5973d4ccc
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2013-11-08 13:42:14 -0800

    factoring out Document.find() logic into a DocumentFinder class

--- lib/ae_page_objects/document.rb
+++ lib/ae_page_objects/document.rb
@@ -2,76 +2,12 @@ module AePageObjects
   class Document < Node
     include Concerns::Visitable
 
-    class FindConditions
-      def initialize(conditions, block_condition)
-        @conditions = conditions || {}
-        @conditions[:block] = block_condition if block_condition
-      end
-
-      def match?(page)
-        @conditions.each do |type, value|
-          case type
-          when :title then
-            return false unless browser.title.include?(value)
-          when :url then
-            return false unless page.current_url.include?(value)
-          when :block then
-            return false unless value.call(page)
-          end
-        end
-
-        true
-      end
-    end
-
     class << self
-
-      def find(conditions = {}, &extra_condition)
-        original_window = AePageObjects::Window.current
-
-        # Loop through all the windows and attempt to instantiate the Document. Continue to loop around
-        # until finding a Document that can be instantiated or timing out.
-        Capybara.wait_until do
-          find_window(FindConditions.new(conditions, extra_condition))
-        end
-
-      rescue Capybara::TimeoutError
-        original_window.switch_to
-
-        all_windows = AePageObjects::Window.all.map do |window|
-          name = window.current_document && window.current_document.to_s || "<none>"
-          {:window_handle => window.handle, :document => name }
-        end
-
-        raise PageNotFound, "Couldn't find page #{self.name} in any of the open windows: #{all_windows.inspect}"
+      def find(*args, &block)
+        DocumentFinder.new(self).find(*args, &block)
       end
 
     private
-
-      def find_window(conditions)
-        AePageObjects::Window.all.each do |window|
-          window.switch_to
-
-          if inst = attempt_to_load(conditions)
-            return inst
-          end
-        end
-
-        nil
-      end
-
-      def attempt_to_load(conditions)
-        inst = new
-
-        if conditions.match?(inst)
-          return inst
-        end
-
-        nil
-      rescue AePageObjects::LoadingFailed
-        # These will happen from the new() call above.
-        nil
-      end
 
       def site
         @site ||= AePageObjects::Site.from(self)
