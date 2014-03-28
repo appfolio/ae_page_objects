cb67d00b9b9f20e2c8b061525b88b8939c24cbc9
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2014-03-28 15:30:40 -0700

    Changed Waiter to not use Timeout

--- lib/ae_page_objects/util/waiter.rb
+++ lib/ae_page_objects/util/waiter.rb
@@ -1,12 +1,20 @@
 module AePageObjects
   module Waiter
     def self.wait_for(&block)
-      Timeout.timeout(Capybara.default_wait_time) do
-        sleep(0.05) until value = Capybara.using_wait_time(0, &block)
-        value
+      seconds_to_wait = Capybara.default_wait_time
+      start_time      = Time.now
+
+      until result = Capybara.using_wait_time(0, &block)
+        delay = seconds_to_wait - (Time.now - start_time)
+
+        if delay <= 0
+          return false
+        end
+
+        sleep(0.05)
       end
-    rescue Timeout::Error
-      false
+
+      result
     end
   end
 end
