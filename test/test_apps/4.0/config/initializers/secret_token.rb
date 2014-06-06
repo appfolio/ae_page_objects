80581f9f1a2bfaed6ae0d5ffd1bd8fb4d4459003
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2014-06-06 15:38:48 -0700

    adding default Rails 4.0.4 app

---
+++ test/test_apps/4.0/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bleh::Application.config.secret_key_base = '3b3bfb0dd3b5a41982691b6dca1a114ee2026fc70cfa1e773613aff2ed4271b36e066067a4d97a49330620c40d7069458eee559b43f8b2de8e1604a36fa4f7a2'
