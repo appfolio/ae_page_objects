eacee6cb0662ff3927d2b01d76b96e2bd9ad3edd
yunlei <yunlei.liu@appfolio.com>
2014-10-12 20:29:40 -0700

    drop engine support for rails versions lower then 3.1

--- test/test_apps/shared/routes.rb
+++ test/test_apps/shared/routes.rb
@@ -1,5 +1,7 @@
 TestApp::Application.routes.draw do
-  mount Forum::Engine, :at => "/forum", :as => "forum_engine"
+  if Rails::VERSION::STRING.to_f > 3.0
+    mount Forum::Engine, :at => "/forum", :as => "forum_engine"
+  end
 
   resources :books, :authors
   root :to => "books#index"
