c21649b51b271abf351397a1e3146071beb0f672
ipmsteven <steven.lyl147@gmail.com>
2014-10-29 01:48:14 -0700

    add ae_page_objects object model graph and update README

---
+++ etc/aepos_object_model.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+require 'ae_page_objects'
+
+COLON = ':'.freeze
+UNDERSCORE = '_'.freeze
+TAB = "\t".freeze
+NAMESPACE = 'AePageObjects'.freeze
+
+# Colons are invalid characters in DOT nodes.
+# Replace them with underscores.
+# http://www.graphviz.org/doc/info/lang.html
+def nodize(klass)
+  klass.name.tr(COLON, UNDERSCORE)
+end
+
+nodes, edges = {}, {}
+
+twitter_objects = ObjectSpace.each_object(Class).select do |klass|
+  klass.name.to_s.start_with?(NAMESPACE)
+end
+
+twitter_objects.each do |klass|
+  loop do
+    unless klass.nil? || klass.superclass.nil? || klass.name.empty?
+      nodes[nodize(klass)] = klass.name
+      edges[nodize(klass)] = nodize(klass.superclass)
+    end
+    klass = klass.superclass
+    break if klass.nil?
+  end
+end
+
+edges.delete(nil)
+
+def puts(object, indent = 0, tab = TAB)
+  super(tab * indent + object)
+end
+
+puts 'digraph classes {'
+# Add or remove DOT formatting options here
+puts "graph [rotate=0, rankdir=\"LR\"]", 1
+puts "node [fillcolor=\"#c4ddec\", style=\"filled\", fontname=\"Helvetica Neue\"]", 1
+puts "edge [color=\"#444444\"]", 1
+nodes.sort.each do |node, label|
+  puts "#{node} [label=\"#{label}\"]", 1
+end
+edges.sort.each do |child, parent|
+  puts "#{child} -> #{parent}", 1
+end
+puts '}'
