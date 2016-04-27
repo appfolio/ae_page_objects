536ed74a9025c8a9631c71aac8ac68683199f729
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2016-04-26 20:22:44 -0700

    Add poll_until to replace Waiter methods
    (cherry picked from commit 607fc8c0cec391450b2482419b78d2e6abd32f45)

--- test/unit/ae_page_objects_test.rb
+++ test/unit/ae_page_objects_test.rb
@@ -41,5 +41,24 @@ class AePageObjectsTest < AePageObjectsTestCase
     end
     assert_equal "Time appears to be frozen", raised.message
   end
-end
 
+  def test_poll_until__with_mocks
+    AePageObjects.expects(:wait_until).yields
+    Capybara.expects(:using_wait_time).with(0).yields
+
+    block = mock
+    block.expects(:called).times(1)
+
+    AePageObjects.poll_until do
+      block.called
+    end
+  end
+
+  def test_poll_until__without_mocks
+    result = AePageObjects.poll_until do
+      :hello
+    end
+
+    assert_equal :hello, result
+  end
+end
