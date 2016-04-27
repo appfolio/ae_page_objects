66abf15f75dcf52d207b60eef8eefea12c3fd933
Donnie Tognazzini <donnie.tognazzini@appfolio.com>
2016-04-26 22:01:40 -0700

    Deprecating all of Waiter in favor of AePageObjects.poll_until

--- lib/ae_page_objects/util/waiter.rb
+++ lib/ae_page_objects/util/waiter.rb
@@ -1,19 +1,27 @@
 module AePageObjects
   module Waiter
-    def self.wait_until(timeout = nil, &block)
-      wait_until!(timeout, &block)
-    rescue WaitTimeoutError
-      false
-    end
+    class << self
+      def wait_until(timeout = nil, &block)
+        warn "[DEPRECATION WARNING]: AePageObjects::Waiter.wait_until is deprecated and will be removed in version 2.0.0. Use AePageObjects.poll_until instead."
+        wait_until_return_false(timeout, &block)
+      end
 
-    def self.wait_for(*args, &block)
-      warn "[DEPRECATION WARNING]: AePageObjects::Waiter.wait_for is deprecated and will be removed in version 2.0.0. Use AePageObjects::Waiter.wait_until instead."
-      wait_until(*args, &block)
-    end
+      def wait_for(timeout = nil, &block)
+        warn "[DEPRECATION WARNING]: AePageObjects::Waiter.wait_for is deprecated and will be removed in version 2.0.0. Use AePageObjects.poll_until instead."
+        wait_until_return_false(timeout, &block)
+      end
 
-    def self.wait_until!(timeout = nil, &block)
-      AePageObjects.wait_until(timeout) do
-        Capybara.using_wait_time(0, &block)
+      def wait_until!(timeout = nil, &block)
+        warn "[DEPRECATION WARNING]: AePageObjects::Waiter.wait_until! is deprecated and will be removed in version 2.0.0. Use AePageObjects.poll_until instead."
+        AePageObjects.poll_until(timeout, &block)
+      end
+
+      private
+
+      def wait_until_return_false(timeout, &block)
+        AePageObjects.poll_until(timeout, &block)
+      rescue WaitTimeoutError
+        false
       end
     end
   end
