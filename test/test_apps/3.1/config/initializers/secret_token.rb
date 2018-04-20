5fc3302a626188d8b03a638ca4f42c14c4f269a1
Tim Connor <tim.connor@appfolio.com>
2018-04-20 14:53:26 -0700

    remove rails 3 appraisals

--- test/test_apps/3.1/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TestApp::Application.config.secret_token = 'de6dd0e38318e853a766f48983e218794f9b8193d44f7b7e6bf27a27382e8e828813e587a43634af746b4d59b16f005f506234a72f37bd5b27259cd25a1062b5'
