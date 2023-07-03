2a3331f87476a6d0e155c21ec61674490891249e
Sarah Sehr <sarah.sehr@appfolio.com>
2023-07-03 16:30:31 -0700

    Delete test app for Rails 5

--- test/test_apps/shared/rails5_models/book.rb
+++
@@ -1,8 +0,0 @@
-class Book < ApplicationRecord
-  belongs_to :author
-  has_one :index
-  accepts_nested_attributes_for :index
-  accepts_nested_attributes_for :author
-
-  validates_presence_of :title
-end
--- test/test_apps/shared/rails5_models/index.rb
+++
@@ -1,4 +0,0 @@
-class Index < ApplicationRecord
-  belongs_to :book
-  validates_presence_of :book
-end
